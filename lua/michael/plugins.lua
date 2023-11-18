a2c609635d14671f6acba629c5b2d0883285428e
Michael Duren <michaeld@michaelduren.com>
2023-11-17 20:03:12 -0600

    add noice to conf, unsure if I'll stay with it

--- lua/michael/plugins.lua
+++ lua/michael/plugins.lua
@@ -186,6 +186,22 @@ local plugins = {
 			vim.fn["mkdp#util#install"]()
 		end,
 	},
+	-- noice
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		opts = {
+			-- add any options here
+		},
+		dependencies = {
+			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+			"MunifTanjim/nui.nvim",
+			-- OPTIONAL:
+			--   `nvim-notify` is only needed, if you want to use the notification view.
+			--   If not available, we use `mini` as the fallback
+			"rcarriga/nvim-notify",
+		},
+	},
 }
 
 require("lazy").setup(plugins)
