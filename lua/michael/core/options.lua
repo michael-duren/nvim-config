8b80c730294c9b940985dfa90dd93130a287abf4
Michael Duren <michaeld@michaelduren.com>
2023-11-18 20:08:04 -0600

    update rust conf, key-b, and opts

--- lua/michael/core/options.lua
+++ lua/michael/core/options.lua
@@ -13,30 +13,34 @@ vim.g.mapleader = " "
 
 -- stop vim from autocommenting next line
 vim.api.nvim_create_autocmd("BufEnter", {
-	callback = function()
-		vim.opt.formatoptions = vim.opt.formatoptions - { "c", "r", "o" }
-	end,
+  callback = function()
+    vim.opt.formatoptions = vim.opt.formatoptions - { "c", "r", "o" }
+  end,
 })
 
 -- 4 tab spacing for cs files
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = "cs",
-	callback = function()
-		vim.opt_local.tabstop = 4
-		vim.opt_local.shiftwidth = 4
-		vim.opt_local.softtabstop = 4
-	end,
+  pattern = "cs",
+  callback = function()
+    vim.opt_local.tabstop = 4
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.softtabstop = 4
+  end,
 })
 
 -- 4 tab spacing for rs files
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = "rs",
-	callback = function()
-		vim.opt_local.tabstop = 4
-		vim.opt_local.shiftwidth = 4
-		vim.opt_local.softtabstop = 4
-	end,
+  pattern = "rs",
+  callback = function()
+    vim.opt_local.tabstop = 4
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.softtabstop = 4
+  end,
 })
 
 -- copy to and from system clipboard
 vim.o.clipboard = "unnamedplus"
+
+-- set log location
+-- vim.opt.logfile = "/Users/michaelduren/.config/nvim/tmp/nvim.log"
+-- vim.cmd.set("logfile=/Users/michaelduren/.config/nvim/tmp/nvim.log")
