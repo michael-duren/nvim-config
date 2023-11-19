503bb93e7235a08eaf9bd19a9d17dc2bd9262188
Michael Duren <michaeld@michaelduren.com>
2023-11-19 08:35:26 -0600

    fix terminal bindings for mult terminals

--- lua/michael/plugin-config/toggleterm.lua
+++ lua/michael/plugin-config/toggleterm.lua
@@ -1,27 +1,31 @@
-require("toggleterm").setup {
-    size = 20,
-    open_mapping = [[<c-\>]],
-    shade_filetypes = {},
-    shade_terminals = true,
-    shading_factor = 1,
-    start_in_insert = true,
-    insert_mappings = true,
-    persist_size = true,
-    direction = "horizontal",
-    close_on_exit = true,
-    shell = vim.o.shell,
-    float_opts = {
-        border = "curved",
-        winblend = 3,
-        highlights = {
-        border = "Normal",
-        background = "Normal",
-        },
-    },
-}
+require("toggleterm").setup({
+	size = 20,
+	open_mapping = [[<c-\>]],
+	shade_filetypes = {},
+	shade_terminals = true,
+	shading_factor = 1,
+	start_in_insert = true,
+	insert_mappings = true,
+	persist_size = true,
+	direction = "horizontal",
+	close_on_exit = true,
+	shell = vim.o.shell,
+	float_opts = {
+		border = "curved",
+		winblend = 3,
+		highlights = {
+			border = "Normal",
+			background = "Normal",
+		},
+	},
+})
 
-for i = 1, 5 do
-    vim.keymap.set('t', '<C-'..i..'>', function()
-        require("toggleterm").exec_command("ToggleTerm"..i)
-    end, {noremap = true, silent = true})
+-- create mappings for 3 terminals
+for i = 1, 3 do
+	vim.api.nvim_set_keymap(
+		"n",
+		"<leader>" .. i,
+		"<cmd> ToggleTerm " .. i .. "<CR>",
+		{ noremap = true, silent = false }
+	)
 end
