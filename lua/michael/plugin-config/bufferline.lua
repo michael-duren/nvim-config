0b6f2f542851e0547120028477b690f819778bda
Michael Duren <michaeld@michaelduren.com>
2023-11-21 11:02:26 -0600

    update bufferline

--- lua/michael/plugin-config/bufferline.lua
+++ lua/michael/plugin-config/bufferline.lua
@@ -1,47 +1,53 @@
-require('bufferline').setup({
+local icons = require("michael.core.icons")
+
+require("bufferline").setup({
   options = {
-    mode = 'buffer',
-    numbers = 'none',
-    close_command = 'bdelete! %d',
-    right_mouse_command = 'bdelete! %d',
-    left_mouse_command = 'buffer %d',
+    mode = "buffer",
+    numbers = "none",
+    close_command = "bdelete! %d",
+    right_mouse_command = "bdelete! %d",
+    left_mouse_command = "buffer %d",
     middle_mouse_command = nil,
-    indicator_icon = '▎',
-    buffer_close_icon = '',
-    modified_icon = '●',
-    close_icon = '',
-    left_trunc_marker = '',
-    right_trunc_marker = '',
+    indicator = { icon = icons.ui.LineMiddle, style = "icon" },
+    buffer_close_icon = icons.ui.Close,
+    modified_icon = icons.git.LineModified,
+    close_icon = icons.ui.Close,
+    left_trunc_marker = icons.ui.ArrowCircleLeft,
+    right_trunc_marker = icons.ui.ArrowCircleRight,
     name_formatter = function(buf) -- buf contains a "name", "path" and "bufnr"
-      if buf.name:match('%.md') then
-        return vim.fn.fnamemodify(buf.name, ':t:r')
+      if buf.name:match("%.md") then
+        return vim.fn.fnamemodify(buf.name, ":t:r")
       end
     end,
     max_name_length = 18,
     max_prefix_length = 15, -- prefix used when a buffer is deduplicated
     tab_size = 18,
-    diagnostics = false,
-    diagnostics_indicator = function(count, level, diagnostics_dict, context)
-      return '(' .. count .. ')'
+    diagnostics = "nvim_lsp",
+    diagnostics_indicator = function(count)
+      return "(" .. count .. ")"
     end,
     offsets = {
       {
-        filetype = 'NvimTree',
-        text = 'File Explorer',
-        text_align = 'center',
+        filetype = "NvimTree",
+        text = "File Explorer",
+        text_align = "center",
       },
     },
     show_buffer_icons = true,
-    show_buffer_close_icons = false,
+    show_buffer_close_icons = true,
     show_close_icon = false,
     show_tab_indicators = true,
     persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
     -- can also be a table containing 2 custom separators
     -- [focused and unfocused]. eg: { '|', '|' }
-    separator_style = 'thin',
-    enforce_regular_tabs = false,
+    separator_style = "thin",
+    enforce_regular_tabs = true,
     always_show_bufferline = true,
-    sort_by = 'id',
+    sort_by = "id",
+    hover = {
+      enabled = true,
+      delay = 200,
+      reveal = { "close" },
+    },
   },
 })
-
