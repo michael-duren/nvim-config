ac6affdab107c9c37b47b5666c6e34c615bf6e87
Michael Duren <michaeld@michaelduren.com>
2023-11-25 10:36:32 -0600

    fix alpha config

--- lua/michael/plugin-config/alpha.lua
+++ lua/michael/plugin-config/alpha.lua
@@ -6,16 +6,17 @@ local icons = require("michael.core.icons")
 dashboard.section.header.val = banner.banner
 
 dashboard.section.buttons.val = {
-  dashboard.button("n", "  > New file", "<CMD>ene!<CR>"),
-  dashboard.button("f", "  > Find file", "<CMD>Telescope find_files<CR>"),
-  dashboard.button("r", "  > Recent", ":Telescope oldfiles<CR>"),
-  dashboard.button("s", "  > Settings", ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
-  dashboard.button("q", "  > Quit NVIM", ":qa<CR>"),
+	dashboard.button("n", icons.ui.NewFile .. "  > New file", "<CMD>ene!<CR>"),
+	dashboard.button("f", icons.ui.File .. " > Find file", "<CMD>Telescope find_files<CR>"),
+	dashboard.button("r", icons.ui.Files .. "  > Recent", ":Telescope oldfiles<CR>"),
+	dashboard.button("h", icons.ui.FindFile .. "  > Harpoon", ":lua require('harpoon.ui').toggle_quick_menu()<CR>"),
+	dashboard.button("s", icons.ui.Gear .. "  > Settings", ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
+	dashboard.button("q", icons.ui.BoldClose .. " > Quit NVIM", ":qa<CR>"),
 }
 
 dashboard.section.footer.val = {
-  icons.misc.Heart .. " Configured by Michael Duren " .. icons.diagnostics.Hint,
-  dashboard.lsp_client_names,
+	icons.misc.Heart .. " Configured by Michael Duren " .. icons.diagnostics.Hint,
+	dashboard.lsp_client_names,
 }
 
 alpha.setup(dashboard.opts)
