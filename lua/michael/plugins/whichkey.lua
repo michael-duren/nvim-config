fd30cfbf4b78a7fc4887d25746e8978b02bab020
Michael Duren <michaeld@michaelduren.com>
2023-11-27 19:04:16 -0600

    update whichkey config

--- lua/michael/plugins/whichkey.lua
+++ lua/michael/plugins/whichkey.lua
@@ -1,1 +1,30 @@
-local wk = require("which-key").setup()
+local wk = require("which-key")
+
+wk.setup({
+	show_help = false,
+	plugins = { spelling = true },
+	key_labels = {
+		["<leader>"] = "SPC",
+	},
+	triggers = "auto",
+})
+
+local groups = {
+	{ key = "C", name = "+Copilot" },
+	{ key = "d", name = "+Debug" },
+	{ key = "f", name = "+Find" },
+	{ key = "g", name = "+Git" },
+	{ key = "K", name = "+Kill" },
+	{ key = "l", name = "+LSP" },
+	{ key = "m", name = "+Markdown" },
+	{ key = "n", name = "+Notifications" },
+	{ key = "x", name = "+Diagnostics" },
+}
+
+for _, group in ipairs(groups) do
+	wk.register({
+		[group.key] = {
+			name = group.name,
+		},
+	}, { prefix = "<leader>" })
+end
