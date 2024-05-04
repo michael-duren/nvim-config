ee6ac8f414c6644df4aefaec2c8c84aad18954c7
Michael Duren <michaeld@michaelduren.com>
2024-05-04 13:31:17 -0500

    remove old plugins

--- lua/michael/plugins_old/ccc.lua
+++
@@ -1,7 +0,0 @@
-local ok, ccc = pcall(require, "ccc")
-
-if not ok then
-	error("Failed to load ccc: " .. ccc)
-end
-
-ccc.setup()
--- lua/michael/plugins_old/colorizer.lua
+++
@@ -1,9 +0,0 @@
-require("colorizer").setup({ "*" }, {
-	default_options = {
-		RRGGBBAA = true, -- #RRGGBBAA hex codes
-		rgb_fn = true, -- CSS rgb() and rgba() functions
-		hsl_fn = true, -- CSS hsl() and hsla() functions
-		css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-		css_fn = true,
-	},
-})
